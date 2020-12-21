5969c1c10ff68a49c6745312ce5f65fbf0a803b7
Alejandro Mery <amery@linux.com>
2020-12-21 01:43:47 +0000

    docker-alpine-mariadb: install my.cnf and improve db creation slightly
    Signed-off-by: Alejandro Mery <amery@linux.com>

--- alpine/mariadb/mysql.sh
+++ alpine/mariadb/mysql.sh
@@ -6,42 +6,58 @@ set -eu
 U=mysql
 R=/var/run/mysqld
 D=/var/lib/mysql
+H=$(hostname)
 
-[ -d "$R" ] || mkdir /var/run/mysqld
+[ -d "$R" ] || mkdir "$R"
+ln -snf /dev/stderr "$D/mysql.err"
 chown -R $U:$U "$R" "$D"
 
-if [ ! -d "$D/mysql" ]; then
-	mysql_install_db --user=$U --ldata=$D
+escape() {
+	echo "$1" | sed -e 's|\([;"\\]\)|\\\1|g' -e "s|'|''|g"
+}
 
-	if [ -z "${MYSQL_ROOT_PASSWORD:-}" ]; then
-		MYSQL_ROOT_PASSWORD="$(pwgen 16 1)"
-	fi
+if [ ! -d "$D/mysql" ]; then
+
+	: ${MYSQL_ROOT_PASSWORD:="$(pwgen 16 1)"}
 
 	F=$(mktemp)
 
 	cat <<-EOT > "$F"
-	USE mysql
-	FLUSH PRIVILEGES ;
-	GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
-	GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
-	SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
-	DROP DATABASE IF EXISTS test ;
-	FLUSH PRIVILEGES ;
+	USE mysql;
+	FLUSH PRIVILEGES;
+
+	GRANT ALL ON *.* TO 'root'@'%' identified by '$(escape "$MYSQL_ROOT_PASSWORD")' WITH GRANT OPTION;
+	DELETE FROM user WHERE host = '$H';
+	DELETE FROM proxies_priv WHERE host = '$H';
 	EOT
 
 	if [ -n "${MYSQL_DATABASE:-}" ]; then
 
+		: ${MYSQL_CHARSET:=utf8}
+		: ${MYSQL_COLLATION:=utf8_general_ci}
+
 		cat <<-EOT
 
-		CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET ${MYSQL_CHARSET:-utf8} COLLATE ${MYSQL_COLLATION:-utf8_general_ci};"
+		CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET $MYSQL_CHARSET COLLATE $MYSQL_COLLATION;
 		EOT
 
 		if [ -n "${MYSQL_USER:-}" ]; then
+
+			: ${MYSQL_PASSWORD:=}
+
 			cat <<-EOT
-			GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '${MYSQL_PASSWORD:-}'
+			GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' identified by '$(escape "$MYSQL_PASSWORD")';
 			EOT
 		fi
 	fi >> "$F"
 
-	/usr/bin/mysqld_safe --user=$U --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < "$F"
+	cat <<-EOT >> "$F"
+	FLUSH PRIVILEGES;
+	DROP DATABASE IF EXISTS test;
+	EOT
+
+	grep -hn ^ "$F"
+	set -x
+	mysql_install_db --user="$U" > /dev/null
+	exec mysqld --user "$U" --bootstrap < "$F"
 fi
