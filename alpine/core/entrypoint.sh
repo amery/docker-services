af3d5c5cd3e49f36627574500b425917d3c950dd
Alejandro Mery <amery@linux.com>
2021-01-03 22:08:48 +0000

    docker-alpine-core: create ${USER_NAME:-appuser} if USER_UID is given
    * ${USER_UID}
    * ${USER_NAME:=appuser}
    * ${USER_GID:=$USER_UID}
    * ${USER_HOME:=/home/$USER_NAME}
    * ${USER_SHELL:=/sbin/nologin}
    
    Signed-off-by: Alejandro Mery <amery@linux.com>

--- alpine/core/entrypoint.sh
+++ alpine/core/entrypoint.sh
@@ -7,6 +7,24 @@ else
 fi
 
 set -eu
+
+if [ -n "${USER_UID:-}" ]; then
+	export USER_NAME="${USER_NAME:-appuser}"
+	export USER_GID="${USER_GID:-$USER_UID}"
+	export USER_HOME="${USER_HOME:-/home/$USER_NAME}"
+	export USER_SHELL="${USER_SHELL:-/sbin/nologin}"
+
+	addgroup -S -g "$USER_GID" "$USER_NAME"
+
+	opt="-S -D"
+	[ ! -d "$USER_HOME" ] || opt="$opt -H"
+
+	adduser $opt \
+		-s "$USER_SHELL" -h "$USER_HOME" \
+		-G "$USER_NAME" -g "$USER_NAME"  \
+		-u "$USER_UID" "$USER_NAME"
+fi
+
 for x in /etc/entrypoint.d/*; do
 	if [ -f "$x" -a -x "$x" ]; then
 		echo "$0: $x"
