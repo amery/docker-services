64754652f1532a1a7ce024fc6e2b0d4e78e9ee70
Alejandro Mery <amery@linux.com>
2023-08-19 21:26:17 +0000

    docker-alpine-php8: add php 8.1 service
    Signed-off-by: Alejandro Mery <amery@linux.com>

---
+++ alpine/php8/8.1/00-prepare-php8.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -eu
+
+# composer wrapper
+F=/usr/bin/composer
+cat <<EOT > "$F"
+#!/bin/sh
+
+set -eu
+exec run-user php "/usr/bin/composer${COMPOSER_PREFERRED:-2}" "\$@"
+EOT
+chmod +x "$F"
+
+# error_log
+chown -R $USER_NAME /var/log/php81
